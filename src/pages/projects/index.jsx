c57bb5e81c3ee03e0119e5d31bd0909d8e4e20de
vihugocosta <victorhugocosta02@gmail.com>
2025-03-09 12:10:27 -0300

    Update index.jsx
    Images fix

--- src/pages/projects/index.jsx
+++ src/pages/projects/index.jsx
@@ -13,14 +13,14 @@ export default function Projects() {
         <div id="projects-list">
           <div className="project-item">
             <h2>Formulário ViaCEP</h2>
-            <img src="/src/assets/form.png" alt="Formulário ViaCEP" />
+            <img src="https://github.com/vihugocosta/portfolio-react/blob/main/src/assets/form.png" alt="Formulário ViaCEP" />
             <p>Formulário de cadastro de usuário com validação de CEP via API do ViaCEP.</p>
             <a href="https://github.com/vihugocosta/viacep-form-example">Ver no GitHub</a>
             <a href="https://viacep-form-victor.netlify.app/">Visitar projeto</a>
           </div>
           <div className="project-item">
             <h2>Portfólio 1.0</h2>
-            <img src="/src/assets/port.png" alt="Portfólio 1.0" />
+            <img src="/https://github.com/vihugocosta/portfolio-react/blob/main/src/assets/port.png" alt="Portfólio 1.0" />
             <p>Meu primeiro portfólio criado com HTML e CSS.</p>
             <a href="https://github.com/vihugocosta/portfolio-dev">Ver no GitHub</a>
             <a href="https://portfolio-dev-victor.netlify.app/">Visitar projeto</a>
